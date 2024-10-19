e2e568d12f6c9efe76d59bb2a45d98b6623c23c7
adithya <adithya.3035@gmail.com>
2024-10-19 00:29:32 -0700

    yo

---
+++ letmetalktohuman/src/views/application/Application.jsx
@@ -0,0 +1,11 @@
+import React from 'react';  
+import './AppStyles.css';
+
+export default function Component() {
+    return (
+      <div>
+        <h1>Welcome to the application page!</h1>
+      </div>
+    );
+  }
+  
