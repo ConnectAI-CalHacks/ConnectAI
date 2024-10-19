8a39f83cbd8a0d55c736756c532628f48f69f2ce
Anirudh Sivakumar <156978613+ani-sivaa@users.noreply.github.com>
2024-10-19 13:47:06 -0700

    Added overview section, updated styles.

--- letmetalktohuman/src/views/landing-page/LandingPage.jsx
+++ letmetalktohuman/src/views/landing-page/LandingPage.jsx
@@ -4,13 +4,13 @@ import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import { ReactTyped as Typed } from 'react-typed';
 import ProfilePic from './assets/output-onlinegiftools-ezgif.com-resize.gif';
-
+import Overview from './overview/Overview'
 
 const stringsToRender = [
   'Talk to a real service agent.',
   'Get past automated customer service.',
   'Save time.',
-  '<strong>Talk to a Human.<!/strong>',
+  'Talk to a Human FAST.',
   ''
 
   
@@ -30,48 +30,9 @@ const LandingPage = () => {
   };
 
   return (
+    <>
     <div className="landing-container">
-      {/* Particles background */}
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: { color: '#0d47a1' }, // Blue background
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: { enable: true, mode: 'push' },
-              onHover: { enable: true, mode: 'repulse' },
-              resize: true,
-            },
-            modes: {
-              push: { quantity: 4 },
-              repulse: { distance: 200, duration: 0.4 },
-            },
-          },
-          particles: {
-            color: { value: '#ffffff' },
-            links: {
-              color: '#ffffff',
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-              outModes: { default: 'bounce' },
-            },
-            number: { value: 80, density: { enable: true, area: 800 } },
-            opacity: { value: 0.5 },
-            shape: { type: 'circle' },
-            size: { value: { min: 1, max: 5 } },
-          },
-          detectRetina: true,
-        }}
-      />
+      
 
       <header className="landing-content">
        
@@ -92,6 +53,9 @@ const LandingPage = () => {
           Learn More
         </button>
     </div>
+
+    <Overview></Overview>
+    </>
   );
 };
 
